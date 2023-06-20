1c1f3ff21df89c93c28e0ad009df34bfbbf02386
Nikita <nikitakemper555@gmail.com>
2023-06-20 18:23:12 +0300

    feat: add input logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log(process.argv.slice(2));
+import { stdin, stdout } from 'process';
+
+const username = process.argv.find((arg) => arg.startsWith('--username')).split('=')[1];
+console.log(`Welcome to the File Manager, ${username}!`);
+
+stdin.on('data',  data => {
+  data.toString().trim() === '.exit' && exit();
+});
+
+process.on('SIGINT', () => exit());
+
+const exit = () => {
+  stdout.write(`Thank you for using File Manager, ${username}, goodbye!\n`);
+  process.exit();
+};
