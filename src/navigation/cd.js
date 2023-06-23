69a6b6bec1ae654cc93bb0c184672f986805000f
Nikita <nikitakemper555@gmail.com>
2023-06-23 12:18:12 +0300

    fix: add invalid input case

--- src/navigation/cd.js
+++ src/navigation/cd.js
@@ -1,21 +1,28 @@
 import path from 'path';
 import { access, stat } from 'fs/promises';
 
-const cd = async (pathTo) => {
+const cd = async (pathTo = '') => {
+  const pathToDirectory = path.join(process.cwd(), pathTo);
   try {
     if (path.isAbsolute(pathTo)) {
       await access(pathTo);
-      process.chdir(pathTo);
     } else {
-      const pathToDirectory = path.join(process.cwd(), pathTo);
       await access(pathToDirectory);
-
-      const stats = await stat(pathTo);
-      if (stats.isDirectory()) {
-        process.chdir(pathToDirectory);
-      } else {
+      const stats = await stat(pathToDirectory);
+      if (!stats.isDirectory()) {
         throw new Error('Not Directory');
       }
+    }
+  } catch (error) {
+    console.log('Invalid input');
+    return;
+  }
+
+  try {
+    if (path.isAbsolute(pathTo)) {
+      process.chdir(pathTo);
+    } else {
+      process.chdir(pathToDirectory);
     }
   } catch (error) {
     console.log('Operation failed');
