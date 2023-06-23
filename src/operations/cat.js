d4baee6d735139fd1c7a3e9c8cfc3885ad198edf
Nikita <nikitakemper555@gmail.com>
2023-06-23 12:18:29 +0300

    fix: add invalid input case

--- src/operations/cat.js
+++ src/operations/cat.js
@@ -1,9 +1,18 @@
 import path from 'path';
+import { access } from 'fs/promises';
 
 import { readFileAsync } from '../utils/readFileAsync.js';
 
-const cat = async (pathTo) => {
+const cat = async (pathTo = '') => {
   const pathToFile = path.join(process.cwd(), pathTo);
+
+  try {
+    if (path.extname(pathToFile) === '') throw new Error('Not File');
+    await access(pathToFile);
+  } catch (error) {
+    console.log('Invalid input');
+    return;
+  }
 
   try {
     const data = await readFileAsync(pathToFile);
